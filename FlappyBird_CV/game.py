c6a3a276a2b29506995b8375cf539fa8b828ffd3
Louis2602 <trantunglam135790@gmail.com>
2022-02-20 23:53:20 +0700

    add flappy bird game using head move

---
+++ FlappyBird_CV/game.py
@@ -0,0 +1,179 @@
+import sys
+import time
+import random
+import pygame
+import imutils
+from collections import deque
+import cv2 as cv
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+pygame.init()
+
+VID_CAP = cv.VideoCapture(0)
+window_size = (VID_CAP.get(cv.CAP_PROP_FRAME_WIDTH),
+               VID_CAP.get(cv.CAP_PROP_FRAME_HEIGHT))
+screen = pygame.display.set_mode(window_size)
+
+# Bird and pipe initialize
+bird_img = pygame.image.load('./img/bird_sprite.png')
+bird_img = pygame.transform.scale(
+    bird_img, (bird_img.get_width() / 10, bird_img.get_height() / 10))
+bird_frame = bird_img.get_rect()
+bird_frame.center = (window_size[0] // 10, window_size[1] // 6)
+
+pipe_frames = deque()
+pipe_img = pygame.image.load('./img/pipe_sprite_single.png')
+pipe_starting_template = pipe_img.get_rect()
+space_between_pipes = 170
+
+
+# Game variables
+game_clock = time.time()
+stage = 1
+pipeSpawnTimer = 0
+time_between_pipe_spawn = 40
+dist_between_pipes = 500
+def pipe_velocity(): return dist_between_pipes / time_between_pipe_spawn
+
+
+score = 0
+didUpdateScore = False
+game_is_running = True
+
+
+with mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
+    while True:
+        if not game_is_running:
+            text = pygame.font.SysFont("Helvetica Bold.ttf", 64).render(
+                "Game Over!", True, (99, 245, 255))
+            tr = text.get_rect()
+            tr.center = (window_size[0]/2, window_size[1]/2)
+            screen.blit(text, tr)
+            pygame.display.update()
+            pygame.time.wait(2000)
+            VID_CAP.release()
+            cv.destroyAllWindows()
+            pygame.quit()
+            sys.exit()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                VID_CAP.release()
+                cv.destroyAllWindows()
+                pygame.quit()
+                sys.exit()
+
+        ret, frame = VID_CAP.read()
+
+        if not ret:
+            print("Empty frame, continuing...")
+            continue
+
+        # Clear screen
+        screen.fill((125, 220, 232))
+
+        frame.flags.writeable = False
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        results = face_mesh.process(frame)
+        frame.flags.writeable = True
+
+        # if results.multi_face_landmarks:
+        #     for face_landmarks in results.multi_face_landmarks:
+        #         mp_drawing.draw_landmarks(image=frame,
+        #                                   landmark_list=face_landmarks,
+        #                                   connections=mp_face_mesh.FACEMESH_TESSELATION,
+        #                                   landmark_drawing_spec=None,
+        #                                   connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
+
+        #         mp_drawing.draw_landmarks(image=frame,
+        #                                   landmark_list=face_landmarks,
+        #                                   connections=mp_face_mesh.FACEMESH_CONTOURS,
+        #                                   landmark_drawing_spec=None,
+        #                                   connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
+
+        #         mp_drawing.draw_landmarks(image=frame,
+        #                                   landmark_list=face_landmarks,
+        #                                   connections=mp_face_mesh.FACEMESH_IRISES,
+        #                                   landmark_drawing_spec=None,
+        #                                   connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style())
+
+        if results.multi_face_landmarks and len(results.multi_face_landmarks) > 0:
+            marker = results.multi_face_landmarks[0].landmark[94].y
+            bird_frame.centery = (marker - 0.5) * 1.5 * \
+                window_size[1] + window_size[1] / 2
+            if bird_frame.top < 0:
+                bird_frame.y = 0
+            if bird_frame.bottom > window_size[1]:
+                bird_frame.y = window_size[1] - bird_frame.height
+
+        # Mirror frame, swap axes because opencv != pygame
+        frame = cv.flip(frame, 1).swapaxes(0, 1)
+
+        # Update pipe positions
+        for pf in pipe_frames:
+            pf[0].x -= pipe_velocity()  # bottom pipe
+            pf[1].x -= pipe_velocity()  # top pipe
+
+        if len(pipe_frames) > 0 and pipe_frames[0][0].right < 0:
+            pipe_frames.popleft()
+
+        # Update screen
+        pygame.surfarray.blit_array(screen, frame)
+        screen.blit(bird_img, bird_frame)
+        checker = True
+        for pf in pipe_frames:
+            # Check if bird went through to update score
+            if pf[0].left <= bird_frame.x <= pf[0].right:
+                checker = False
+                if not didUpdateScore:
+                    score += 1
+                    didUpdateScore = True
+            screen.blit(pipe_img, pf[1])
+            screen.blit(pygame.transform.flip(pipe_img, 0, 1), pf[0])
+        if checker:
+            didUpdateScore = False
+
+        # Stage, score text
+        text = pygame.font.SysFont("Helvetica Bold.ttf", 50).render(
+            f'Stage {stage}', True, (99, 245, 255))
+        tr = text.get_rect()
+        tr.center = (100, 50)
+        screen.blit(text, tr)
+
+        text = pygame.font.SysFont("Helvetica Bold.ttf", 50).render(
+            f'Score {score}', True, (99, 245, 255))
+        tr = text.get_rect()
+        tr.center = (100, 100)
+        screen.blit(text, tr)
+
+        pygame.display.flip()
+
+        # Check if bird is touching a pipe
+        if (any([bird_frame.colliderect(pf[0]) or bird_frame.colliderect(pf[1]) for pf in pipe_frames])):
+            game_is_running = False
+
+        # Time to add new pipes
+        if pipeSpawnTimer == 0:
+            top = pipe_starting_template.copy()
+            top.x, top.y = window_size[0], random.randint(
+                100 - 1000, window_size[1] - 100 - space_between_pipes - 1000)
+
+            bottom = pipe_starting_template.copy()
+            bottom.x, bottom.y = window_size[0], top.y + \
+                1000 + space_between_pipes
+            pipe_frames.append([top, bottom])
+
+        # Update pipe spawn timer - make it cyclical
+        pipeSpawnTimer += 1
+        if pipeSpawnTimer >= time_between_pipe_spawn:
+            pipeSpawnTimer = 0
+
+        if time.time() - game_clock >= 10:
+            time_between_pipe_spawn *= 5/6
+            stage += 1
+            game_clock = time.time()
